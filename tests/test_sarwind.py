7a4a47e846893d076836a24c8bc79c15ee6329c7
Frode Dinessen <froded@pc5464.pc.met.no>
2022-10-26 16:27:56 +0200

    Added a new test for _get_aux_wind_from_str

--- tests/test_sarwind.py
+++ tests/test_sarwind.py
@@ -1,9 +1,10 @@
 import os
 import pytest
 
-from nansat.nansat import Nansat
 
-from sarwind.sarwind import get_nansat
+#from sarwind.sarwind import get_nansat
+from sarwind import get_nansat
+import sarwind
 
 # Se oppsett og eksempler i https://github.com/metno/discovery-metadata-catalog-ingestor/
 # ang testing. Evt søk i google...
@@ -13,14 +14,26 @@ sar_file = 'http://nbstds.met.no/thredds/fileServer/NBS/S1A/2021/03/24/EW/S1A_EW
 arome_file = 'https://thredds.met.no/thredds/fileServer/aromearcticarchive/2021/03/24/arome_arctic_vtk_20210324T03Z.nc'
 
 @pytest.mark.sarwind
-def testSARWind__get_nansat__returns__nansat(filesDir):
+def testSARWind__get_nansat__returns__nansat(filesDir,mocker):
     """Tests that get_nansat returns a Nansat object for both the
     sample datasets.
     """
     # Sample datasets
     sar_ds = os.path.join(filesDir, 'S1A_EW_GRDM_1SDH_20210324T035507_20210324T035612_037135_045F42_5B4C.nc')
     arome_ds = os.path.join(filesDir, 'arome_arctic_vtk_20210324T03Z.nc')
+    class NansatMocked:
+        pass
+    mocker.patch("sarwind.Nansat", return_value=NansatMocked())
     n = get_nansat(sar_ds)
-    assert type(n) is Nansat
+    assert type(n) is NansatMocked
     w = get_nansat(arome_ds)
-    assert type(w) is Nansat
+    assert type(w) is NansatMocked
+
+def testSARWind__get_aux_wind_from_str__returns__wdir(filesDir, mocker):
+    arome_ds = os.path.join(filesDir, 'arome_arctic_vtk_20210324T03Z.nc')
+    class wdirMocked:
+        pass
+
+    mocker.patch("sarwind.SARWind._get_aux_wind_from_str", return_value=wdirMocked())
+    n = sarwind.SARWind._get_aux_wind_from_str(arome_ds)
+    assert type(n) is wdirMocked
