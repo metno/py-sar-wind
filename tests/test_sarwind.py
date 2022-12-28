150679907eb221309160245b4b443b7113b7f84b
Morten W. Hansen <morten.wergeland.hansen@gmail.com>
2022-12-28 10:15:40 +0100

    #4: created marker for unit tests in github actions ci

--- tests/test_sarwind.py
+++ tests/test_sarwind.py
@@ -3,18 +3,19 @@ import pytest
 from sarwind.sarwind import SARWind
 
 
-# @pytest.mark.sarwind
-# def testSARWind__get_aux_wind_from_str__returns__wdir(arome, mocker):
-#     """ Test that calls function _get_aux_wind_from_str.
-#     """
-#
-#     class wdirMocked:
-#         pass
-#
-#     mocker.patch("sarwind.sarwind.SARWind._get_aux_wind_from_str", return_value=wdirMocked())
-#     print(arome)
-#     n = SARWind._get_aux_wind_from_str(arome)
-#     assert type(n) is wdirMocked
+@pytest.mark.unittests
+@pytest.mark.sarwind
+def testSARWind__get_aux_wind_from_str__returns__wdir(arome, mocker):
+    """ Test that calls function _get_aux_wind_from_str.
+    """
+
+    class wdirMocked:
+        pass
+
+    mocker.patch("sarwind.sarwind.SARWind._get_aux_wind_from_str", return_value=wdirMocked())
+    print(arome)
+    n = SARWind._get_aux_wind_from_str(arome)
+    assert type(n) is wdirMocked
 
 
 @pytest.mark.nbs
@@ -28,23 +29,23 @@ def testSARWind_using_s1EWnc_arome_filenames(sarEW_NBS, arome):
     assert type(w) == SARWind
 
 
-# @pytest.mark.safe
-# @pytest.mark.sarwind
-# def testSARWind_using_s1EWsafe_meps_filenames(sarEW_SAFE, meps):
-#     """ Test that wind is generated from Sentinel-1 data in EW-mode,
-#     HH-polarization and SAFE based netcdf file, with direction from
-#     the MEPS model.
-#     """
-#     w = SARWind(sarEW_SAFE, meps)
-#     assert type(w) == SARWind
-#
-#
-# @pytest.mark.safe
-# @pytest.mark.sarwind
-# def testSARWind_using_s1IWDVsafe_meps_filenames(sarIW_SAFE, meps):
-#     """ Test that wind is generated from Sentinel-1 data in IW-mode,
-#     VV-polarization and SAFE based netcdf file, with wind direction
-#     from MEPS model.
-#     """
-#     w = SARWind(sarIW_SAFE, meps)
-#     assert type(w) == SARWind
+@pytest.mark.safe
+@pytest.mark.sarwind
+def testSARWind_using_s1EWsafe_meps_filenames(sarEW_SAFE, meps):
+    """ Test that wind is generated from Sentinel-1 data in EW-mode,
+    HH-polarization and SAFE based netcdf file, with direction from
+    the MEPS model.
+    """
+    w = SARWind(sarEW_SAFE, meps)
+    assert type(w) == SARWind
+
+
+@pytest.mark.safe
+@pytest.mark.sarwind
+def testSARWind_using_s1IWDVsafe_meps_filenames(sarIW_SAFE, meps):
+    """ Test that wind is generated from Sentinel-1 data in IW-mode,
+    VV-polarization and SAFE based netcdf file, with wind direction
+    from MEPS model.
+    """
+    w = SARWind(sarIW_SAFE, meps)
+    assert type(w) == SARWind
