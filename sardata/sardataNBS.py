8c46d3812d096cb16975510d1768f97a0441dc81
froded <froded@met.no>
2023-02-10 14:47:52 +0100

    Corrected som errors

--- sardata/sardataNBS.py
+++ sardata/sardataNBS.py
@@ -1,52 +1,47 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+""" License: This file is part of https://github.com/metno/met-sar-vind
+             met-sar-vind is licensed under the Apache-2.0 license
+             (https://github.com/metno/met-sar-vind/blob/main/LICENSE).
 """
-Created on Thu Feb  9 09:32:05 2023
-
-@author: froded
-"""
-
 from owslib import fes
 from owslib.fes import SortBy, SortProperty
 from owslib.csw import CatalogueServiceWeb
 
-
 class SARData():
     """
-    A class for getting Sentinel-1 netCDF data from the Norwegian Ground Segment (NBS)
+     A class for getting Sentinel-1 netCDF data from the Norwegian Ground Segment (NBS)
 
-    Parameters
-    -----------
-    endpoint : str
+     Parameters
+     -----------
+     endpoint : str
                URL to NBS. Default: https://nbs.csw.met.no/csw
 
-    bbox     : int list
-               A boudary box for search area speified in latitude and longitude 
+     bbox     : int list
+               A boudary box for search area speified in latitude and longitude
                bbox = [lon_min, lat_min, lon_max, lat_max]
-               
+
                Example:
                >>> bbox = [-10, 75, 40, 85]
-               
-    start    : datetime object
-               Specify the start time of the range to serchthe 
-               
-    stop     :  datetime object
-               Specify the stop time of the range to serchthe  
-               
+
+     start    : datetime object
+               Specify the start time of the range to serchthe
+
+     stop     :  datetime object
+               Specify the stop time of the range to serchthe
+
                Example:
                >>> from datetime import datetime, timedelta
                >>> stop = datetime(2010, 1, 1, 12, 30, 59).replace(tzinfo=pytz.utc)
                >>> start = stop - timedelta(days=7)
-               
-    kw_names : str
-               A search string filter to limit the result of a search. 
+
+     kw_names : str
+               A search string filter to limit the result of a search.
                Example:
-               >>> kw_name='S1A*'    
-               
+               >>> kw_name='S1A*'
+
     """
 
-    def __init__(self, endpoint='https://nbs.csw.met.no/csw', bbox=None, start=None, stop=None, kw_names=None, crs="urn:ogc:def:crs:OGC:1.3:CRS84", *args, **kwargs):
-        
+    def __init__(self, endpoint='https://nbs.csw.met.no/csw', bbox=None, start=None, stop=None, 
+                 kw_names=None, crs='urn:ogc:def:crs:OGC:1.3:CRS84', *args, **kwargs):
         constraints = []
         csw = None
         while csw is None:
@@ -55,44 +50,39 @@ class SARData():
                 csw = CatalogueServiceWeb(endpoint, timeout=60)
             except:
                 pass
-        
+
         if kw_names:
             kw = dict(wildCard="*", escapeChar="\\", singleChar="?", propertyname="apiso:AnyText")
-            or_filt = fes.Or([fes.PropertyIsLike(literal=("*%s*" % val), **kw) for val in kw_names])
+            or_filt = fes.Or([
+                fes.PropertyIsLike(literal=("*%s*" % val), **kw) for val in kw_names])
             constraints.append(or_filt)
-        
-        
-        
-        if all(v is not None for v in [start, stop]): 
+
+        if all(v is not None for v in [start, stop]):
             begin, end = self._fes_date_filter(start, stop)
             constraints.append(begin)
             constraints.append(end)
-        
+
         if bbox:
             bbox_crs = fes.BBox(bbox, crs=crs)
             constraints.append(bbox_crs)
         if len(constraints) >= 2:
-            filter_list = [
-                fes.And(
-                    constraints
-                    )
-            ]
+            filter_list = [fes.And(constraints)]
         else:
             filter_list = constraints
-            
+
         self._get_csw_records(csw, filter_list, pagesize=2, maxrecords=10)
         self.csw = csw
         url_opendap = []
-        
+
         for key, value in list(csw.records.items()):
             for ref in value.references:
                 if ref['scheme'] == 'OPENDAP:OPENDAP':
                     url_opendap.append(ref['url'])
         self.url_opendap = url_opendap
-            
-       
-        
- 
+
+
+
+
     def _get_csw_records(self, csw, filter_list, pagesize=2, maxrecords=10):
         """Iterate `maxrecords`/`pagesize` times until the requested value in
         `maxrecords` is reached.
@@ -116,17 +106,14 @@ class SARData():
             if startposition >= maxrecords:
                 break
         csw.records.update(csw_records)
-            
-    
-    
-    
+
     def _fes_date_filter(self, start, stop, constraint="within"):
         """
         Take datetime-like objects and returns a fes filter for date range
         (begin and end inclusive).
         NOTE: Truncates the minutes!!!
         """
-       
+
         start = start.strftime("%Y-%m-%d %H:00")
         stop = stop.strftime("%Y-%m-%d %H:00")
         if constraint == "overlaps":
@@ -146,14 +133,3 @@ class SARData():
         else:
             raise NameError("Unrecognized constraint {}".format(constraint))
         return begin, end
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
- 
