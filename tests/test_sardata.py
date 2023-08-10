2de93ade688fdaa6eb019a0db5bd5f357faaa979
frodedinessen <froded@met.no>
2023-08-10 14:42:11 +0200

    Changed OPENDAP:OPENDAP to OPeNDAP:OPeNDAP

--- tests/test_sardata.py
+++ tests/test_sardata.py
@@ -13,9 +13,9 @@ def testSARData_input_parameter():
     """ Test setting up CSW cronstrains from input parameters
     """
     """ Freetext search """
-    kw_names = 'S1A*'
+    kw_names = 'S1A%'
     or_filt = SARData._get_freetxt_search([], kw_names)
-    assert type(or_filt) == fes.Or
+    assert type(or_filt) == fes.PropertyIsLike
 
     """ Restricting search from start,stop time """
     stop = datetime.datetime(2022, 1, 8, 5, 30, 59).replace(tzinfo=pytz.utc)
@@ -45,18 +45,18 @@ def testSARData_from_NBS(mock_csw, mock_get_csw_records):
         references = []
 
     myrec_val = Myval_class
-    myrec_val.references = [{'scheme': 'OPENDAP:OPENDAP',
-                             'url': 'https://nbstds.met.no/thredds/dodsC/NBS/S2B/2022/02/08/\
-                             S2B_MSIL1C_20220208T103109_N0400_R108_T34WEB_20220208T125524.nc'},
+    myrec_val.references = [{'scheme': 'OPeNDAP:OPeNDAP',
+                             'url': 'https://nbstds.met.no/thredds/dodsC/NBS/S2B/2022/02/08/'\
+                             'S2B_MSIL1C_20220208T103109_N0400_R108_T34WEB_20220208T125524.nc'},
                             {'scheme': 'OGC:WMS',
-                             'url': 'https://nbswms.met.no/thredds/wms_ql/NBS/S2B/2022/02/08/\
-                             S2B_MSIL1C_20220208T103109_N0400_R108_T34WEB_20220208T125524.nc\
+                             'url': 'https://nbswms.met.no/thredds/wms_ql/NBS/S2B/2022/02/08/'\
+                             'S2B_MSIL1C_20220208T103109_N0400_R108_T34WEB_20220208T125524.nc\
                              ?SERVICE=WMS&REQUEST=GetCapabilities'},
                             {'scheme': 'download',
-                             'url': 'https://nbstds.met.no/thredds/fileServer/NBS/S2B/2022/02/08/\
-                             S2B_MSIL1C_20220208T103109_N0400_R108_T34WEB_20220208T125524.nc'},
+                             'url': 'https://nbstds.met.no/thredds/fileServer/NBS/S2B/2022/02/08/'\
+                             'S2B_MSIL1C_20220208T103109_N0400_R108_T34WEB_20220208T125524.nc'},
                             {'scheme': None,
-                             'url': "https://colhub.met.no/odata/v1/Products(\
+                             'url': "https://colhub.met.no/odata/v1/Products('\
                              '2a34c82f-675a-4304-af4b-b1c3d8030824')/$value"}]
     mycsw = Mycsw_class
     mycsw.records = {'key1': myrec_val}
@@ -65,5 +65,6 @@ def testSARData_from_NBS(mock_csw, mock_get_csw_records):
     mock_get_csw_records.return_value = mycsw()
     for (key, val) in list(mycsw.records.items()):
         sw = SARData()
-        assert sw.url_opendap[0] == 'https://nbstds.met.no/thredds/dodsC/NBS/S2B/2022/02/08/\
-        S2B_MSIL1C_20220208T103109_N0400_R108_T34WEB_20220208T125524.nc'
+        print(sw.url_opendap[0])
+        assert sw.url_opendap[0] == 'https://nbstds.met.no/thredds/dodsC/NBS/S2B/2022/02/08/'\
+                'S2B_MSIL1C_20220208T103109_N0400_R108_T34WEB_20220208T125524.nc'
