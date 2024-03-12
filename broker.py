bfd024be0f88f3b723f75900931cb13d05be5081
frodedinessen <froded@met.no>
2024-03-12 11:18:18 +0100

    Reading config parameters from config.json file

--- broker.py
+++ broker.py
@@ -12,9 +12,10 @@ import os
 import fnmatch
 from nansat.nansat import Nansat
 import subprocess
+import sys
+import json
 
 os.environ.setdefault('GDAL_ENABLE_DEPRECATED_DRIVER_DODS','YES')
-
 
 class Broker():
     """
@@ -25,7 +26,7 @@ class Broker():
      Parameters
      -----------
      endpoint : str
-               URL to Data. Default: https://csw.s-enda-dev.k8s.met.no/csw
+               URL to Data. Default: https://csw.s-enda.k8s.met.no/csw
 
      bbox     : int list
                A boudary box for search area speified in latitude and longitude
@@ -53,14 +54,30 @@ class Broker():
 
     def __init__(self, bbox=[-25, 60, 70, 85], start=None, stop=None):
         # Set start and stop value to today if not provided as argument
-        if not isinstance(start, datetime) or not isinstance(stop, datetime):
+        if ((not isinstance(start, datetime)) or (not isinstance(stop, datetime))):
             warnings.warn('Start/stop time not provided. Using current time as default!')
             start = datetime.now().replace(tzinfo=pytz.utc)
             stop = start + timedelta(days=1)
+
+        # Reading config.json file
+        try:
+            json_file = open('./config.json', 'r') 
+            conf = json.load(json_file)
+        except Exception as e:
+                print('Could not reads config.json file')
+                print(e)
+                sys.exit()    
+                
         print('\nSearch for new Sentinel-1 data between start: %s stop: %s' % (start.strftime('%Y%m%dT%H'), stop.strftime('%Y%m%dT%H')))
         self.start = start
         self.stop = stop
-        self.outpath = '/data/sar_wind_products/%04d/%02d/' % (start.year,start.month)
+        self.outpath = '%s/%04d/%02d/' % (conf['outpath'], start.year,start.month)
+        self.scp = conf['scp']
+        self.scp_path = conf['scp_path']
+        self.scp_host = conf['scp_host']
+        self.scp_user = conf['scp_user']
+        self.id_rsa = conf['id_rsa']
+        
         if not os.path.exists(self.outpath):
             os.makedirs(self.outpath)
 
@@ -68,18 +85,17 @@ class Broker():
         self.nc_filenames = []
 
         # Find available Sentinel-1 data in NBS
-        self.sar_freetext = '%1A_EW%'
+        self.sar_freetext = '%1A_%'
         self.model_freetext = 'Arome-Arctic%'
         self.bbox = bbox
         self.wind = ''
         self.main()
+        
 
     def main(self):
         #  Search NBS for Sentinel-1 data
-        #  NB! currently no check if data has alredy been processed.
         sar = SARData(start=self.start, stop=self.stop, bbox=self.bbox, kw_names=self.sar_freetext)
-        # print(sar.url_opendap)
-
+        
         if len(sar.url_opendap) == 0:
             raise Exception('No SAR data available for the requested date/time')
 
@@ -93,36 +109,45 @@ class Broker():
             # Looking for model wind data
             try:
                 modelUrl = self._get_arome_file(sarfile, self.bbox)
+                if modelUrl == '':
+                    print('Not able to find model wind data matching %s' % (sarfile))
+                    print('Move to next sar file \n\n')
+                    continue
             except Exception as e:
                 print(e)
                 modelUrl = ''
-                print('Not able to find arome data matching %s' % (sarfile))
+                print('Error getting model wind data matching %s' % (sarfile))
+                print('Move to next sar file \n\n')
                 continue
 
             # Start running CMOD wind processing
-            print('Start processing on:')
+            print('\n Start processing on:')
             print('SAR file: %s' % sarfile)
             print('Model file: %s' % modelUrl)
             try:
                 sw = SARWind(sarfile, modelUrl)
                 ncfilename = '%s/%s_wind.nc' % (self.outpath, os.path.basename(sarfile).split('.')[0])
-                sw.export(ncfilename, bands=[22, 23, 24, 25])
-                # sw.plot()
+                
+                #sw.export(ncfilename, bands=[22, 23, 24, 25])
+                sw.export2netcdf(filename = ncfilename, bands=None)
+                 # sw.plot()
                 self.sw_filenames.append(sarfile)
                 self.nc_filenames.append(ncfilename)
                 
-                dstpath = '/lustre/storeB/project/fou/fd/project/sar-wind/products/%04d/%02d/%02d' % (self.start.year,self.start.month, self.start.day)
-                cmd = 'ssh -i /home/ubuntu/.ssh/id_rsa \"froded@ppi-clogin-a1.met.no\" \"mkdir -p %s\"' % dstpath
-                print(cmd)
-                subprocess.call(cmd, shell=True)
-
-
-                cmd = 'scp -i /home/ubuntu/.ssh/id_rsa %s froded@ppi-clogin-a1.met.no:%s/' % (ncfilename, dstpath)
-                print(cmd)
-                subprocess.call(cmd, shell=True)
-
+                if self.scp == "True":
+                    dstpath = '%s/%04d/%02d/%02d' % (self.scp_path, self.start.year,self.start.month, self.start.day)
+                    try:
+                        cmd = 'ssh -i %s \"%s@%s\" \"mkdir -p %s\"' % (self.id_rsa, self.scp_user, self.scp_host, dstpath)
+                        print(cmd)
+                        subprocess.call(cmd, shell=True)
+                        
+                        cmd = 'scp -i %s %s %s@%s:%s/' % (self.id_rsa, ncfilename, self.scp_user, self.scp_host, dstpath)
+                        print(cmd)
+                        subprocess.call(cmd, shell=True)
+                    except Exception as e:
+                        print(e)
             except:
-                print('Could not generate wind from files')
+                print('Could not generate wind from files:')
                 print('SAR file: %s' % sarfile)
                 print('Arome file: %s' % modelUrl) 
          
@@ -135,6 +160,7 @@ class Broker():
         for m in url_opendap:
             if (m.find(seachStr) > -1):
                 modelUrl = m
+                break
         return modelUrl
 
     def _get_arome_time(self, hour):
@@ -149,25 +175,32 @@ class Broker():
     def _get_arome_file(self, sarfile, bbox):
         nobject = Nansat(sarfile)
         ntime = nobject.time_coverage_end
+        # Get hour closest to model hour %3 
         hour = self._get_arome_time(ntime.hour)
 
         # Need to search 4 days ahead in time to find match of model wind
         modelstart = datetime(ntime.year, ntime.month, ntime.day,
                               int(hour), 0, 0).replace(tzinfo=pytz.utc)
-        modelstop = datetime(ntime.year, ntime.month,
-                             (ntime.day+4), 0, 0, 0).replace(tzinfo=pytz.utc)
-        print(modelstart, modelstop)
+        # Get Arome data from previous day in case modelstart data not available 
+        modelPrevDay = modelstart - timedelta(days=1)
 
-        # Find available NWP model file og continue to next SAR file
+        modelstop = modelstart + timedelta(days=4)
+        
+        print('modelstart,modelstop,modelPrevDay')
+        print(modelstart, modelstop, modelPrevDay)
+
+        # Find available NWP model file within the start and stop time continue to next SAR file
         model_freetext = '%Arome-Arctic'
-        modelwind = WINDdata(start=modelstart, stop=modelstop, bbox=bbox, kw_names=model_freetext)
+
+        modelwind = WINDdata(start=modelPrevDay, stop=modelstop, kw_names=model_freetext)
         if len(modelwind.url_opendap) == 0:
             raise Exception('No arome data available for date %04d-%02d-%02d' %
                             (modelstart.year, modelstart.month, modelstart.day))
 
         modelUrl = ''
         cnt = 0
-        while ((modelUrl == '') and (cnt < 3)):
+        # Search 3 * 3h back in time to find Arome data closest to SAR file date/time
+        while ((modelUrl == '') and (cnt < len(modelwind.url_opendap))):
             previous_time = ((datetime(modelstart.year, modelstart.month, modelstart.day,
                                        modelstart.hour))-\
                              timedelta(hours=cnt*3)).replace(tzinfo=pytz.utc)
@@ -186,12 +219,13 @@ class Broker():
 
 
 if __name__ == '__main__':
-    #start = datetime(2023, 10, 6, 0, 0, 0).replace(tzinfo=pytz.utc)
-    #stop = datetime(2023, 10, 7, 0, 0, 0).replace(tzinfo=pytz.utc)
+    start = datetime(2024, 3, 8, 0, 0, 0).replace(tzinfo=pytz.utc)
+    stop = datetime(2024, 3, 8, 23, 0, 0).replace(tzinfo=pytz.utc)
 
     now  = datetime.now().replace(tzinfo=pytz.utc)
-    start = datetime(now.year, now.month, now.day, 0, 0, 0).replace(tzinfo=pytz.utc)
-    stop = start + timedelta(days=1)
+    #start = datetime(now.year, now.month, now.day, 0, 0, 0).replace(tzinfo=pytz.utc)
+    #stop = datetime(now.year, now.month, now.day, 23, 0, 0).replace(tzinfo=pytz.utc)
+    #stop = start + timedelta(days=0)
 
     broker_object = Broker(start=start, stop=stop)
     # broker_object.wind.plot()  
